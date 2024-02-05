e5227b8a31f0e7de1b25427f4d5af1b68566dd5e
ishangupta229 <121247230+ishangupta229@users.noreply.github.com>
2024-02-05 23:15:45 +0530

    Create printdec.java

---
+++ printdec.java
@@ -0,0 +1,14 @@
+public class printdec {
+    public static void printdec(int num) {
+        if(num==1){
+            System.out.println(num);
+            return;
+        }
+        System.out.print(num + " ");
+        printdec(num-1);
+
+    }
+    public static void main(String[] args) {
+        printdec(100);
+    }
+}
