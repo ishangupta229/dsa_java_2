e86fb8fa13388d82de78249d4958b8c1490458a0
ishangupta229 <121247230+ishangupta229@users.noreply.github.com>
2024-03-21 23:27:22 +0530

    Create reversal.java

---
+++ reversal.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.LinkedList;
+public class refersal {
+    public static void reversal(Queue <Integer> q) {
+        Stack <Integer>s = new Stack<>();
+        while (!q.isEmpty()) {
+            s.push(q.remove());            
+        }        
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+    }
+
+    public static void print(Queue <Integer> q) {
+        while (!q.isEmpty()) {
+            System.out.print(q.remove() + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Queue <Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        print(q);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        reversal(q);
+        print(q);
+    }
+}
